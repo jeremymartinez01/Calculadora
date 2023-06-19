8c7312d3a804a140db2d22983eb8ce0da14e8464
Jeremy Martinez <jjmartin@espol.edu.ec>
2023-06-18 19:34:11 -0500

    agregando elemento horas extra y cantidad de horas extra con funcionalidad adaptada para estos nuevos elementos

--- public/javascript/calculadora.js
+++ public/javascript/calculadora.js
@@ -3,9 +3,11 @@ let funcionPredeterminada = () => {
     formulario.addEventListener('submit', (evento) => {
         evento.preventDefault();
         let valorPorHora= document.getElementById('valorPorHora').value
+        let valorPorHoraExtra= document.getElementById('valorPorHoraExtra').value
         let interfazhoras= document.getElementById('interfazHoras').value
         let testinghora= document.getElementById('testinghoras').value
-        let resultado= (valorPorHora*interfazhoras) + (valorPorHora*testinghora)
+        let cantidadhorasextra= document.getElementById('horasextras').value
+        let resultado= (valorPorHora*interfazhoras) + (valorPorHora*testinghora) + (valorPorHoraExtra*cantidadhorasextra)
         document.getElementById('valorTotal').value= resultado
       })    
 }
